511ed47447932c14e965aa85786fb137fb5d2898
Noelle Darcy <noellerdarcy@gmail.com>
2022-11-30 10:33:52 -0500

    new day

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
