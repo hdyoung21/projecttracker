165e00676a07fd80ca37e5165732976b4a0b4cd8
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2022-11-30 10:39:36 -0500

    delete?

---
+++ middleware/errors.js
@@ -0,0 +1,13 @@
+import {StatusCodes} from 'http-status-codes'
+
+const errorsMiddleware = (err, req, res, next) => {
+    console.log(err)
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: 'try again later'
+    }
+    
+    res.status(defaultError.statusCode).json({ msg: err })
+}
+
+export default errorsMiddleware
